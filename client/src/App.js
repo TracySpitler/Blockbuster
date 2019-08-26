69dd69a99349dbd1994ad6b8a2850043285f7d1e
Tracy Spitler <tracyspitler@gmail.com>
2019-08-26 19:50:28 -0400

    updated App.js - imported and utilized Routes.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,15 @@ import './App.css';
 
 //React Router
 import { BrowserRouter as Router } from 'react-router-dom'
+import Routes from './routes/Routes.js';
 
 class App extends Component {
 
   render() {
     return (
       <Router>
-        <div className="App container-fluid p-0 m-0">
-
-          <div className="card card-cascade header">
-            <div className="view view-cascade gradient-card-header gradient py-5 rounded-bottom">
-              <h1 className="card-header-title mb-3">Your Movie List</h1>
-              <form className="form-inline text-center py-2 d-flex justify-content-center">
-                <i className="fas fa-search" aria-hidden="true"></i>
-                <input className="search form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
-              </form>
-            </div>
-          </div>
-
-          <div className="container d-flex justify-content-start">
-            <h5 className="ml-3 mt-5"><strong>Recently Added</strong></h5>
-          </div>
-
+        <div>
+          <Routes />
         </div>
       </Router>
     );
