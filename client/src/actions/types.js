4527d74b945643786c3a48ad38998a3b4c1913bf
Tracy Spitler <tracyspitler@gmail.com>
2019-10-26 23:06:18 -0400

    updated client/src/actions/types.js - exported all types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,5 @@
 export const GET_MOVIES = 'GET_MOVIES';
 export const ADD_MOVIE = 'ADD_MOVIE';
-export const DELETE_MOVIE = 'DELETE_MOVIE';
+export const KEEP_MOVIE = 'KEEP_MOVIE';
+export const REMOVE_MOVIE = 'REMOVE_MOVIE';
+export const GET_INFO = "GET_INFO";
