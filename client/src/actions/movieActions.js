602ccddbf0cf9b4f3446261c5d829e9682875143
Tracy Spitler <tracyspitler@gmail.com>
2019-10-26 23:07:05 -0400

    updated client/src/actions/movieActions.js - created actions to CRUD data

--- client/src/actions/movieActions.js
+++ client/src/actions/movieActions.js
@@ -1,7 +1,55 @@
-import {  GET_MOVIES, ADD_MOVIE, DELETE_MOVIE } from './types';
+import {  GET_MOVIES, ADD_MOVIE, KEEP_MOVIE, REMOVE_MOVIE } from './types';
+import axios from 'axios';
 
-export const getMovies = () => {
-  return {
-    type: GET_MOVIES
-  };
+// action to set the GET_MOVIES state
+export const getMovies = () => dispatch => {
+  //dispatch(setItemsLoading());
+  axios
+    .get('/api/movies')
+    .then(res =>
+      dispatch({
+        type: GET_MOVIES,
+        // send all movies
+        payload: res.data
+      })
+    );
+};
+
+// action to ADD_MOVIE to the server
+export const addMovie = (movie) => dispatch => {
+  axios
+    .post('/api/movies', movie)
+    .then(res =>
+      dispatch({
+        type: ADD_MOVIE,
+        // send the added movie
+        payload: res.data
+      })
+    );
+};
+
+// action to KEEP_MOVIE from the list
+export const keepMovie = (id) => dispatch => {
+  axios
+    .post(`/api/movies/${id}`)
+    .then(res =>
+      dispatch({
+        type: KEEP_MOVIE,
+        // send the id
+        payload: id
+      })
+    );
+};
+
+// action to REMOVE_MOVIE from the list
+export const removeMovie = (id) => dispatch => {
+  axios
+    .delete(`/api/movies/${id}`)
+    .then(res =>
+      dispatch({
+        type: REMOVE_MOVIE,
+        // send the id
+        payload: id,
+      })
+    );
 };
