ca49606c57f73e5aff2400fa7777d9a744c20e66
Tracy Spitler <tracyspitler@gmail.com>
2019-10-26 23:10:34 -0400

    changed Movies.js to GoThrough.js

--- client/src/components/GoThrough.js
+++ client/src/components/GoThrough.js
@@ -1,47 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getMovies } from '../actions/movieActions';
-import PropTypes from 'prop-types';
+import { getMovies, removeMovie, keepMovie } from '../actions/movieActions';
 
-class Movies extends Component {
+class GoThrough extends Component {
 
-  // load the movies from store.js
   componentDidMount() {
     this.props.getMovies();
   }
 
+  handleRemove = (e) => {
+    this.props.removeMovie(e.target.id);
+  }
+
+  handleKeep = (e) => {
+    this.props.keepMovie(e.target.id);
+  }
+
   render() {
-    const backdrop = "http://image.tmdb.org/t/p/original/";
     const movies = this.props.movies.gothrough;
 
     const movieList = movies.length ? (
       movies.map(movie => {
+        const poster = "http://image.tmdb.org/t/p/original/" + movie.poster_path;
+        const youtube = "http://www.youtube.com/watch_popup?v=" + movie.video_url;
         return (
-            <div key={movie.id} className="my-3 col-lg-4 col-md-6 col-sm-12">
+            <div key={movie._id} className="my-3 col-lg-4 col-md-6 col-sm-12">
               <div className="card">
                 <div className="view overlay rounded-top">
-                  <img className="card-img-top" src={backdrop + movie.backdrop_path} alt={movie.title}/>
-                  <a href={movie.video_url}>
+                  <img className="card-img-top" src={poster} alt={movie.title}/>
+                  <a className="video" href={youtube}>
                     <div className="mask rgba-white-slight"></div>
                   </a>
                 </div>
 
                 <div className="rounded-bottom text-center">
                   <ul className="list-unstyled list-inline font-small d-flex justify-content-around mt-3 mb-0">
-                    <li className="list-inline-item pr-2"><a href={movie.video_url} className="white-text">
-                      <span className="fa-stack fa-lg unwanted">
+                    <li id={movie._id} className="list-inline-item pr-2">
+                      <span className="fa-stack click fa-lg unwanted">
                         <i className="fa fa-circle fa-stack-2x"></i>
-                        <i className="fas fa-ban fa-stack-1x fa-inverse"></i>
-                      </span></a>
+                        <i id={movie._id} onClick={this.handleRemove} className="fas fa-ban fa-stack-1x fa-inverse"></i>
+                      </span>
                     </li>
                     <li className="list-inline-item pr-2"><strong>{movie.title}</strong><br />
                       <p>({movie.year})</p>
                     </li>
-                    <li className="list-inline-item"><a href={movie.video_url} className="white-text">
-                      <span className="fa-stack fa-lg keeper">
+                    <li className="list-inline-item">
+                      <span className="fa-stack click fa-lg keeper">
                         <i className="fa fa-circle fa-stack-2x"></i>
-                        <i className="fas fa-star fa-stack-1x fa-inverse"></i>
-                      </span></a>
+                        <i id={movie._id} onClick={this.handleKeep} className="fas fa-star fa-stack-1x fa-inverse"></i>
+                      </span>
                     </li>
                   </ul>
                 </div>
@@ -69,8 +76,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getMovies: () => { dispatch({ type: 'GET_MOVIES' }) }
+    getMovies: () => { dispatch(getMovies()) },
+    removeMovie: (id) => { dispatch(removeMovie(id)) },
+    keepMovie: (id) => { dispatch(keepMovie(id)) }
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Movies);
+export default connect(mapStateToProps, mapDispatchToProps)(GoThrough);
