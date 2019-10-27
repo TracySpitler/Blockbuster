b12829da264cbda2a33e31032478d1bc08788a32
Tracy Spitler <tracyspitler@gmail.com>
2019-10-26 23:05:23 -0400

    updated client/src/routes/Routes.js - included all routed and respective components

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import Browse from '../views/Browse';
+import GoThrough from '../components/GoThrough';
+import Unwanted from '../components/Unwanted';
+import Keepers from '../components/Keepers';
 import NotFound from '../views/NotFound';
 
 const Routes = () => {
@@ -9,10 +11,13 @@ const Routes = () => {
   return (
 
     <Switch>
-      <Route path="/" exact component={Browse} />
+      <Redirect exact from="/" to="/gothrough" />
+      <Route path="/gothrough" exact component={GoThrough} />
+      <Route path="/unwanted" exact component={Unwanted} />
+      <Route path="/keepers" exact component={Keepers} />
       <Route path="*" component={NotFound} />
     </Switch>
-
+    
   )
 };
 
